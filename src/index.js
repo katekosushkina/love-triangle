1cc0f7241b8bf62b66caba806494da30382943d6
katekosushkina <kate.kosushkina@gmail.com>
2018-09-23 13:37:11 +0300

    done

--- src/index.js
+++ src/index.js
@@ -6,11 +6,10 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
   let arr = [];
   let count=0;
       preferences.forEach(function(item, i) {
-        if (!(arr.indexOf(i) != -1)){
-          //item=2
-            i1=preferences[i]-1; //2-1=1
-            i2=preferences[i1]-1; //3-1 = 2
-            i3=preferences[i2]-1; //1-1 = 0
+        if (arr.indexOf(i) == -1){
+            i1=preferences[i]-1; 
+            i2=preferences[i1]-1; 
+            i3=preferences[i2]-1;
             if (i!=i1 && i===i3){
               count++;
               arr.push(i1, i2, i3);
